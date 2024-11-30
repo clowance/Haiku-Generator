1a23c5ee08a524b10787644cdc204b07f7c81567
clowance <165958501+clowance@users.noreply.github.com>
2024-11-30 16:39:31 -0600

    added api integration js content

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+function displayHaiku(response) {
+  console.log("haiku generated");
+
+  new Typewriter("#haiku", {
+    strings: response.data.answer,
+    autoStart: true,
+  });
+}
+
 function generateHaiku(event) {
   event.preventDefault();
 
-  new Typewriter("#haiku", {
-    strings:
-      "The haiku goes here <br>I hope that it will be good <br>It is worth a try",
-    autoStart: true,
-  });
+  let topicInput = document.querySelector("#user-instructions");
+  let apiKey = "cbfa4f3bo097d8bbfbd63t0a17f12af3";
+  let context =
+    "You are a haiku master and love to write pithy and cleaver haikus based on topics selected by the user. You should generate a haiku in basic HTML format and separate each line with a <br />. Make sure to follow the user instructions for the topic of the haiku. Don't include any quotation marks. Please do not generate a title for the haiku.";
+  let prompt = `User instructions: Generate a haiku about ${topicInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayHaiku);
 }
 
 let haikuFormElement = document.querySelector("#haiku-generator-form");
